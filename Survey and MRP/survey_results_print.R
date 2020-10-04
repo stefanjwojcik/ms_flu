b25f3fe7c2190b53217be446f161ebc75955c1bb
Stefan Wojcik <swojcik@pewresearch.org>
2020-10-04 17:43:54 -0500

    creating print file for printing out survey results

---
+++ Survey and MRP/survey_results_print.R
@@ -0,0 +1,68 @@
+
+# Are there differences between the way the data is loaded?
+
+#############
+source("Survey and MRP/Main_Replication_Source.R")
+
+# MAIN TABLES 
+Main_Table1
+
+Main_pctA1 
+Main_pctA2
+Main_pctB1 
+
+## MAIN FLU MODEL RESULTS 
+# RR
+main_RR 
+main_RR_CI 
+# RD: 
+main_RD 
+main_RD_CI 
+
+Main_Fig1
+
+# VOLUME RESULTS
+VOL_RR 
+VOL_RR_CI 
+# RD: 
+VOL_RD 
+VOL_RD_CI 
+
+# FATHER RESULTS
+DAD_RR 
+DAD_RR_CI 
+# RD: 
+DAD_RD 
+DAD_RD_CI 
+
+
+# SI TABLES 
+SI_Table2
+SI_Table4
+SI_Table5
+SI_Table6
+SI_Table7
+SI_Table8
+SI_Table9
+SI_Table10
+SI_Table11
+SI_Table12
+SI_Table13
+SI_Table14
+SI_Table15
+SI_Table16
+SI_Table17
+SI_Table19
+SI_Table20
+SI_Table21
+SI_Table22
+SI_Table23
+# SI Figures 
+SI_Fig5
+SI_Fig7
+
+# Classification
+class_mod_training_acc
+confmat$table
+confmat$overall[["Accuracy"]]
+SI_ranef_plot
